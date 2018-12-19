b71ef7b7f9eae0b1d6b8b64bcc6d33d64f4adc74
Praveen H <praveen@Praveens-MBP.Dlink>
2018-12-19 15:32:22 +0530

    first commit

---
+++ src/main/java/com/praveen/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,14 @@
+package com.praveen.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
+
